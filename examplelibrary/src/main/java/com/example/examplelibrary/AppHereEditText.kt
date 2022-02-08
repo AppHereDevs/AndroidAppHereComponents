0db5e2ae80a854599259a2d59aca485055a30e35
emre.uygun <emre.uygun@demirorenteknoloji.com>
2022-02-08 15:44:42 +0300

    edited edit text

--- examplelibrary/src/main/java/com/example/examplelibrary/AppHereEditText.kt
+++ examplelibrary/src/main/java/com/example/examplelibrary/AppHereEditText.kt
@@ -1,9 +1,9 @@
 package com.example.examplelibrary
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatEditText
 
 // Created by Emre UYGUN on 8.02.2022
 // Copyriht © Demiroren Teknoloji. All rights reserved.
@@ -12,12 +12,17 @@ class AppHereEditText(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyle: Int = 0
-) : LinearLayout(context, attributeSet, defStyle) {
+) : AppCompatEditText(context, attributeSet, defStyle) {
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.app_here_edit_text, this, false)
+        this.setBackgroundColor(Color.BLACK)
+        this.hint = "yusuf"
     }
+
+//    init {
+//        LayoutInflater.from(context).inflate(R.layout.app_here_edit_text, this, false)
+//    }
 
 }
